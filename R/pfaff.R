b4ee41223dd22e547edea014315bfa59aa899f65
Guillaume FIlion <guillaume.filion@gmail.com>
2012-07-28 14:52:35 +0200

    Fixed coercion to double before calling C in 'pfaff'.

--- R/pfaff.R
+++ R/pfaff.R
@@ -2,5 +2,5 @@ pfaff = function(x) {
    # Test uniformity of the column vectors of matrix |x|.
    stopifnot(is.matrix(x))
    stopifnot(all(dim(x) > 1))
-   return (.Call("uniformity_R_call", x + 0.0))
+   return (.Call("uniformity_R_call", x + double(1)))
 }
